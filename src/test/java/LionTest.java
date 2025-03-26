e3041a094a00a433b0433e6264078498bdadb7f6
Nikolay <nikolay.voronov@leroymerlin.ru>
2025-03-26 21:49:01 +0300

    Add mock tests

---
+++ src/test/java/LionTest.java
@@ -0,0 +1,54 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensTest() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        Mockito.when(feline.getKittens()).thenReturn(3);
+        int actualKittensCount = lion.getKittens();
+        int expectedKittensCount = 3;
+        assertEquals(expectedKittensCount, actualKittensCount);
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> actual = lion.getFood();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void doesHaveManeTest() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        boolean expectedHaveMane = true;
+        boolean actualHaveMane = lion.doesHaveMane();
+        assertEquals(actualHaveMane, expectedHaveMane);
+    }
+
+    @Test
+    public void doesHaveManeThrowsException() {
+        try {
+            Lion lion = new Lion("Котик", feline);
+        } catch (Exception exception) {
+            String textException = "Используйте допустимые значения пола животного - самец или самка";
+            assertEquals(textException, exception.getMessage());
+        }
+    }
+}
